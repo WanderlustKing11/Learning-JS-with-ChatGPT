80ecd3e8f322506e568f2d3ce5b93c3b8bac6cb6
Doug <wanderlustking11@gmail.com>
2024-01-09 14:52:36 -0500

    Add public folder for static files regarding Weather project; successfully fetch api data and receive current weather and icon

--- workingWithServers/public/weather.js
+++ workingWithServers/public/weather.js
@@ -6,4 +6,9 @@ document.addEventListener('DOMContentLoaded', async () => {
   const weatherAlert = document.getElementById('weather');
   const currentWeather = weatherData.weather[0].main;
   weatherAlert.textContent = `${currentWeather}`;
+
+  const icon = weatherData.weather[0].icon;
+  const imageURL = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+  const weatherIcon = document.getElementById('weather-icon');
+  weatherIcon.innerHTML = `<img src=${imageURL}>`;
 });
--- workingWithServers/weatherServer.js
+++ workingWithServers/weatherServer.js
@@ -1,11 +1,17 @@
-import { config } from 'dotenv';
-config();
-import express from 'express';
-import fetch from 'node-fetch';
+require('dotenv').config();
+const express = require('express');
 
+let fetch;
+
+(async () => {
+  fetch = (await import('node-fetch')).default;
+})();
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Fetch Location Data
 async function fetchGeoData(zipCode, countryCode) {
@@ -24,6 +30,7 @@ async function fetchGeoData(zipCode, countryCode) {
 async function fetchWeatherData(lat, lon) {
   const apiKey = process.env.MYKEY;
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+//   console.log(`URL: ${weatherURL}, API Key: ${apiKey}`);
 
   try {
     const response = await fetch(weatherURL);
@@ -38,12 +45,14 @@ app.get('/weather', async (req, res) => {
   const countryCode = 'US';
   const geoData = await fetchGeoData(zipCode, countryCode);
 
+//   console.log('Geo Data:', geoData);
+
   if (geoData) {
     const weatherData = await fetchWeatherData(geoData.lat, geoData.lon);
-    console.log(weatherData);
+    // console.log('Weather Data: ', weatherData);
     res.json(weatherData);
   } else {
-    res.status(500).send("Error fetching weather data");
+    res.status(500).send('Error fetching weather data');
   }
 });
 
